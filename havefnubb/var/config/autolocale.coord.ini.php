b2132e261ed44d4185b7fb9a0ef7f3ac0ef25ada
foxmask <olivier@foxmask.info>
2009-05-07 21:09:39 +0000

    User can now have his forum in his own language when he chooses the right code language in his pref
    --HG--
    extra : convert_revision : svn%3A2597059e-4560-0410-9562-ba342e98936d/trunk%40210

--- havefnubb/var/config/autolocale.coord.ini.php
+++ havefnubb/var/config/autolocale.coord.ini.php
@@ -3,7 +3,7 @@
 
 ; copy this file into the var/config directory of your application
 
-availableLanguageCode = fr_FR,en_US
+availableLanguageCode = fr_FR,en_US,en_EN
 
 ;active la detection du changement de langage via l'url fournie
 enableUrlDetection= on
@@ -13,6 +13,4 @@ urlParamNameLanguage=lang
 
 
 ; utilisation du langage indiqué dans le navigateur
-useDefaultLanguageBrowser = off
-
-
+useDefaultLanguageBrowser = on
--- modules/havefnubb/classes/authhavefnubb.listener.php
+++ modules/havefnubb/classes/authhavefnubb.listener.php
@@ -13,56 +13,71 @@ class authhavefnubbListener extends jEventListener{
    /**
    *
    */
-   function onAuthLogin ($event) {
-      $login = $event->getParam('login');
+    function onAuthLogin ($event) {
+        global $gJConfig;    
+        $login = $event->getParam('login');
 
-      // update the last connection access
-      $dao = jDao::get('havefnubb~member');
-      
-      $user = $dao->getByLogin($login);
-      if (!$user) {
+        // update the last connection access
+        $dao = jDao::get('havefnubb~member');
+
+        $user = $dao->getByLogin($login);
+        if (!$user) {
           die("");
-      }
-      // put the current date
-      $user->member_last_connect = time();
-      $user->connected = time();
-      
-      $dao->update($user);
-   }
-   
-   function onjcommunity_save_account ($event) {
-      jMessage::add(jLocale::get('havefnubb~member.profile.updated'),'ok');
-   }
-   
-   // send a mail to the admin when a user registers
-   function onAuthNewUser ($event) {
-      global $HfnuConfig, $gJConfig;
+        }
+        // put the current date
+        $user->member_last_connect = time();
+        $user->connected = time();
 
-	  $toEmail = ($HfnuConfig->getValue('admin_email','main') != '') ? $HfnuConfig->getValue('admin_email','main') : $gJConfig->mailer['webmasterEmail'];
-	  
-	  if ($toEmail == '') {
-		 throw new jException('havefnubb~mail.email.config.not.done.properly');		 
-	  }
-	  // send an email only if the forum is installed
-	  // this avoid to send an email when the forum is installing
-	  // and send an email to the admin after creating his account
-	  if ( $HfnuConfig->getValue('installed','main') == 1 ) {
-		 $user = $event->getParam('user');
-		 
-		 $mail = new jMailer();
-		 $mail->From       = $gJConfig->mailer['webmasterEmail'];
-		 $mail->FromName   = $gJConfig->mailer['webmasterName'];
-		 $mail->Sender     = $gJConfig->mailer['webmasterEmail'];
-		 $mail->Subject    = jLocale::get('havefnubb~member.registration.new.member.registered');
-		 
-		 $tpl = new jTpl();
-		 $tpl->assign('login',$user->login);
-		 $tpl->assign('server',$_SERVER['SERVER_NAME']);
-		 $mail->Body = $tpl->fetch('havefnubb~warn_new_registration', 'text');
-		 
-		 $mail->AddAddress($toEmail);
-		 $mail->Send();
-	  }
+        $dao->update($user);
+      
+        $_SESSION['JX_LANG'] = $user->member_language;
+        $gJConfig->locale = $user->member_language;
+   }
+   
+    function onAuthLogout($event) {
+        $_SESSION['JX_LANG'] = '';
+        unset($_SESSION['JX_LANG']);
+   }
+   
+    function onjcommunity_save_account ($event) {
+        global $gJConfig;
+        $form = $event->getParam('form');
+        if ( $form->getData('member_language') != '') {
+            $_SESSION['JX_LANG'] = $form->getData('member_language');
+            $gJConfig->locale = $form->getData('member_language');
+        }
+        jMessage::add(jLocale::get('havefnubb~member.profile.updated'),'ok');
+   }
+   
+    // send a mail to the admin when a user registers
+    function onAuthNewUser ($event) {
+        global $HfnuConfig, $gJConfig;
+
+        $toEmail = ($HfnuConfig->getValue('admin_email','main') != '') ? $HfnuConfig->getValue('admin_email','main') : $gJConfig->mailer['webmasterEmail'];
+
+        if ($toEmail == '') {
+            throw new jException('havefnubb~mail.email.config.not.done.properly');		 
+        }
+        // send an email only if the forum is installed
+        // this avoid to send an email when the forum is installing
+        // and send an email to the admin after creating his account
+        if ( $HfnuConfig->getValue('installed','main') == 1 ) {
+            $user = $event->getParam('user');
+
+            $mail = new jMailer();
+            $mail->From       = $gJConfig->mailer['webmasterEmail'];
+            $mail->FromName   = $gJConfig->mailer['webmasterName'];
+            $mail->Sender     = $gJConfig->mailer['webmasterEmail'];
+            $mail->Subject    = jLocale::get('havefnubb~member.registration.new.member.registered');
+
+            $tpl = new jTpl();
+            $tpl->assign('login',$user->login);
+            $tpl->assign('server',$_SERVER['SERVER_NAME']);
+            $mail->Body = $tpl->fetch('havefnubb~warn_new_registration', 'text');
+
+            $mail->AddAddress($toEmail);
+            $mail->Send();
+        }
    }
    
    function onjcommunity_check_before_save_registration($event) {
