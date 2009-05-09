1507d4e51135164a14b03c05e67488490b7fa90e
foxmask <olivier@foxmask.info>
2009-05-09 07:32:21 +0000

    add downloads to the main menu
    --HG--
    extra : convert_revision : svn%3A2597059e-4560-0410-9562-ba342e98936d/trunk%40215

--- modules/havefnubb/classes/hfnumenu.listener.php
+++ modules/havefnubb/classes/hfnumenu.listener.php
@@ -38,7 +38,13 @@ class hfnumenuListener extends jEventListener{
 			jUrl::get('hfnusearch~default:index'),
 			4,
 			'main'));
-		  
+	  
+	  $event->add(new hfnuMenuItem('search',
+			jLocale::get('downloads~common.downloads'),
+			jUrl::get('downloads~default:index'),
+			99,
+			'main'));
+	  
       if ( jAcl2::check('hfnu.admin.index'))    {	  
 		 $event->add(new hfnuMenuItem('admin',
 			jLocale::get('havefnubb~main.admin.panel'),
