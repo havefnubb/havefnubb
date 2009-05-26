63246d37873ae4381631170f7e85a54f841fe2eb
foxmask <olivier@foxmask.info>
2009-05-26 20:43:30 +0000

    ticket #71 + ticket #53
    --HG--
    extra : convert_revision : svn%3A2597059e-4560-0410-9562-ba342e98936d/trunk%40259

--- modules/hfnuinstall/install/update/1.1.0RC3/install.mysql.sql
+++ modules/hfnuinstall/install/update/1.1.0RC3/install.mysql.sql
@@ -3,5 +3,5 @@ ADD INDEX ( forum_type ) ;
 
 ALTER TABLE `hf_forum` ADD `forum_url` varchar( 255 ) DEFAULT NULL ;
 
-
+update `hf_member` set member_status = 0 where member_status = 1;
 update `hf_member` set member_status = 1 where member_status = 2;
