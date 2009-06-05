c34b262bc407dde308f1d0d9a5ce2320e8afc0e2
foxmask <olivier@foxmask.info>
2009-06-05 09:10:49 +0000

    update of hfnucontact install tree + update of the update/1.1.0RC3/install.mysql.sql script
    --HG--
    extra : convert_revision : svn%3A2597059e-4560-0410-9562-ba342e98936d/trunk%40276

--- modules/hfnuinstall/install/update/1.1.0RC3/install.mysql.sql
+++ modules/hfnuinstall/install/update/1.1.0RC3/install.mysql.sql
@@ -9,3 +9,9 @@ update `hf_member` set member_status = 1 where member_status = 2;
 alter table `hf_member` add `member_created` datetime DEFAULT NULL;
 
 update `hf_member` set member_created = member_request_date;
+
+INSERT INTO `hf_jacl2_rights` (`id_aclsbj`, `id_aclgrp`, `id_aclres`) VALUES
+('hfnu.admin.contact', 1, '');
+
+INSERT INTO `hf_jacl2_subject` (`id_aclsbj`, `label_key`) VALUES
+('hfnu.admin.contact', 'hfnucontact~acl2.admin.contact');
