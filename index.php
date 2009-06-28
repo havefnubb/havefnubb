8d84fd7c92376d7de80e53249b1879ec287345a4
foxmask <olivier@foxmask.info>
2009-06-28 12:26:35 +0000

    Ticket #88 : new directory tree
    --HG--
    extra : convert_revision : svn%3A2597059e-4560-0410-9562-ba342e98936d/trunk%40307

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
 * @licence  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
 */
 
-require ('../application.init.php');
+require ('application.init.php');
 require (JELIX_LIB_CORE_PATH.'request/jClassicRequest.class.php');
 
-$config_file = 'install/config.ini.php';
+$config_file = 'index/config.ini.php';
 
 $jelix = new jCoordinator($config_file);
 $jelix->process(new jClassicRequest());
--- jelix-scripts.init.php
+++ jelix-scripts.init.php
@@ -8,14 +8,14 @@
 * @licence  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
 */
 
-require (dirname(__FILE__).'/../lib/jelix/init.php');
+require (dirname(__FILE__).'/lib/jelix/init.php');
 
 define ('JELIX_APP_PATH', dirname (__FILE__).DIRECTORY_SEPARATOR); // don't change
 
-define ('JELIX_APP_TEMP_PATH',    realpath(JELIX_APP_PATH.'../temp/havefnubb-cli/').DIRECTORY_SEPARATOR);
+define ('JELIX_APP_TEMP_PATH',    realpath(JELIX_APP_PATH.'./temp/havefnubb-jelix-scripts/').DIRECTORY_SEPARATOR);
 define ('JELIX_APP_VAR_PATH',     realpath(JELIX_APP_PATH.'./var/').DIRECTORY_SEPARATOR);
 define ('JELIX_APP_LOG_PATH',     realpath(JELIX_APP_PATH.'./var/log/').DIRECTORY_SEPARATOR);
 define ('JELIX_APP_CONFIG_PATH',  realpath(JELIX_APP_PATH.'./var/config/').DIRECTORY_SEPARATOR);
-define ('JELIX_APP_WWW_PATH',     realpath(JELIX_APP_PATH.'./www/').DIRECTORY_SEPARATOR);
+define ('JELIX_APP_WWW_PATH',     realpath(JELIX_APP_PATH).DIRECTORY_SEPARATOR);
 define ('JELIX_APP_CMD_PATH',     realpath(JELIX_APP_PATH.'./scripts/').DIRECTORY_SEPARATOR);
 
