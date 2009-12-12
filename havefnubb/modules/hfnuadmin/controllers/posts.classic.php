8961d1eac61ae5beda9d29c0f5e129eee4aa96a0
foxmask <olivier@foxmask.info>
2009-12-12 23:26:54 +0000

    ticket #126 informé les moderateurs des messages non lus par aucun autre moderateur
    --HG--
    extra : convert_revision : svn%3A2597059e-4560-0410-9562-ba342e98936d/trunk%40454

---
+++ havefnubb/modules/hfnuadmin/controllers/posts.classic.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Controller that handle everything about the posts
+ * 
+* @package   havefnubb
+* @subpackage hfnuadmin
+* @author    FoxMaSk
+* @copyright 2008 FoxMaSk
+* @link      http://havefnubb.org
+* @licence  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
+*/
+
+class postsCtrl extends jController {
+    public $pluginParams = array(
+        '*' => array('auth.required'=>true,
+		    'hfnu.check.installed'=>true,
+		    'banuser.check'=>true,
+                    'jacl2.right'=>'hfnu.admin.index'),		
+    );
+    
+    public function unread() {
+        $rep = $this->getResponse('html');
+        $tpl = new jTpl();
+        $tpl->assign('posts',jClasses::getService('havefnubb~hfnuposts')->findUnreadThreadByMod());
+        $rep->body->assign('MAIN',$tpl->fetch('posts.list'));
+        return $rep;
+    }
+}
