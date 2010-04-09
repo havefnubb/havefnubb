165f70d20b2d330abd57839fda150b936f9f1299
foxmask <olivier@foxmask.info>
2010-04-10 00:36:44 +0200

    fix issue #32 - Display the result and respect the rights access - using the search_index_havefnubb

--- havefnubb/modules/hfnusearch/classes/search_index_havefnubb.class.php
+++ havefnubb/modules/hfnusearch/classes/search_index_havefnubb.class.php
@@ -13,62 +13,56 @@
 jClasses::inc('hfnusearch~search_index');
 class search_index_havefnubb extends search_index {
 	/**
-	 * searchEngineRun methode which make a search from the engine by querying the table define in the dao of the hfnusearch.ini.php file
+	 * default searchEngineRun methode which make a search from the engine by querying the table define in the dao of the hfnusearch.ini.php file
 	 * @param object $event
 	 */
 	function searchEngineRun ($event) {
-
 		$cleaner = jClasses::getService('hfnusearch~cleaner');
 		$words = $cleaner->stemPhrase($event->getParam('string'));
-		$nb_words = count($words);
+		$page = $event->getParam('page');
+		$limit = $event->getParam('limit');
+
 		// no words ; go back with nothing :P
 		if (!$words) {
 			return array('count'=>0,'result'=>array());
 		}
+		//1) open the config file
+		$HfnuSearchConfig  =  new jIniFileModifier(JELIX_APP_CONFIG_PATH.'havefnu.search.ini.php');
+		//2) get the dao we want to read
+		$dataSource = $HfnuSearchConfig->getValue('dao');
+		//3) build an array with each one
+		$dataSources = preg_split('/,/',$dataSource);
+		foreach ($dataSources as $ds) {
+			// due to a bug in jelix parser of ini file, we drop the ~ for nothing and
+			// will be able to find the datasource in our config file
+			$dsCfg = str_replace('~','',$ds);
+			//4) get a factory of the current DAO
+			$dao = jDao::get($ds);
 
-		$id_forum = 0;
-		if ($event->getParam('id_forum') > 0)
-		 $id_forum = (int) $event->getParam('id_forum');
+			//getting the column name on which we need to make the query
+			$indexSubject = $HfnuSearchConfig->getValue('index_subject',$dsCfg);
+			$indexMessage = $HfnuSearchConfig->getValue('index_message',$dsCfg);
 
-		// watch in search_words ...
-		$cnx = jDb::getConnection();
-		$strQuery = 'SELECT DISTINCT id_post, COUNT(*) as nb, SUM(weight) as total_weight, subject, posts.parent_id, message, member_login as login, date_created, forum.id_forum, forum_name, cat_name, category.id_cat FROM '.$cnx->prefixTable('search_words');
-		$strQuery .= ' LEFT JOIN ' . $cnx->prefixTable('posts') .' AS posts  ON posts.id_post  = '.$cnx->prefixTable('search_words').'.id ';
-		$strQuery .= ' LEFT JOIN ' . $cnx->prefixTable('member') .' AS member ON member.id_user = posts.id_user ';
-		$strQuery .= ' LEFT JOIN ' . $cnx->prefixTable('forum') .' AS forum ON forum.id_forum = posts.id_forum ';
-		$strQuery .= ' LEFT JOIN ' . $cnx->prefixTable('category') .' AS category ON category.id_cat = forum.id_cat ';
-		$strQuery .= ' WHERE (';
-		$counter=0;
-		foreach ($words as $word) {
-		  if ($counter > 0 ) $strQuery .= ' OR ';
-		  $strQuery .= " words  = '". addslashes($word)."'";
-		  $counter++;
-		}
+			//5) get all the record
+			$conditions = jDao::createConditions();
+			$conditions->startGroup('AND');
 
-		$strQuery .= ') ';
+			foreach ($words as $word) {
+				$conditions->addCondition($indexSubject,'LIKE','%'.$word.'%');
+				$conditions->addCondition($indexMessage,'LIKE','%'.$word.'%');
+			}
+			$conditions->endGroup();
 
-		if ($id_forum > 0)
-		 $strQuery .= " AND forum.id_forum = '".$id_forum."' ";
+			$allRecord = $dao->findBy($conditions);
+			$record = $dao->findBy($conditions, $page, $limit);
 
-		$strQuery .= ' GROUP BY id_post ';
-
-		// AND query?
-
-		$exact = false;
-		if ($exact)
-		{
-		$strQuery .= ' HAVING nb = '.$nb_words;
-		}
-
-		$strQuery .= ' ORDER BY cat_name ASC, nb DESC, total_weight DESC';
-
-		$rs = $cnx->query($strQuery);
-		$result='';
-
-		while($record = $rs->fetch()){
-		 //let check if the current user can access to the posts we found earlier above
-		 if (jAcl2::check('hfnu.forum.view','forum'.$record->id_forum))
-			$event->Add(array('SearchEngineResult'=>$record));
+			foreach ($record as $rec) {
+				if (jAcl2::check('hfnu.forum.view','forum'.$rec->id_forum))
+					$event->Add(array('SearchEngineResult'=>$rec,
+								  'SearchEngineResultTotal'=>$allRecord->rowCount()
+								  )
+						);
+			}
 		}
 	}
 
