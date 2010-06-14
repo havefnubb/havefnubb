511bd30abf2bd321c6c70712166e173beb4c09e1
Laurent Jouanneau <laurent@xulfr.org>
2010-06-14 18:22:55 +0200

    installer for hfnuthemes
    --HG--
    branch : migration_jx12
    rename : havefnubb/modules/hfnuthemes/install/install.mysql.sql => havefnubb/modules/hfnuthemes/install/sql/postinstall.mysql.sql

--- havefnubb/modules/hfnuthemes/install/install.mysql.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `jacl2_rights` (`id_aclsbj`, `id_aclgrp`, `id_aclres`) VALUES
-('hfnu.admin.index', 1, '');
-INSERT INTO `jacl2_subject` (`id_aclsbj`, `label_key`) VALUES
-('hfnu.admin.index', 'havefnubb~acl2.admin.index');
---
+++ havefnubb/modules/hfnuthemes/install/install.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @package     hafnuthemes
+* @author      Laurent Jouanneau
+* @contributor
+* @copyright   2010 Laurent Jouanneau
+ * @link      http://havefnubb.org
+ * @license  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
+*/
+
+
+class hfnuthemesModuleInstaller extends jInstallerModule {
+
+    function install() {
+
+    }
+}
