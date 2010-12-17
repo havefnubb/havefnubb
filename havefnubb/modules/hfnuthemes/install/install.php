91d2a4becd67d1c19ae8801b3f34d30d91c639cb
foxmask <olivier@foxmask.info>
2010-12-17 15:18:00 +0100

    Back to 793

--- havefnubb/modules/hfnuthemes/install/install.php
+++ havefnubb/modules/hfnuthemes/install/install.php
@@ -1,11 +1,11 @@
 <?php
 /**
-* @package     hfnuthemes
+* @package     hafnuthemes
 * @author      Laurent Jouanneau
 * @contributor
-* @copyright 2010 Laurent Jouanneau
-* @link      http://havefnubb.org
-* @license  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
+* @copyright   2010 Laurent Jouanneau
+ * @link      http://havefnubb.org
+ * @license  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
 */
 
 
