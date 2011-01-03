c892494fa9db5d74cc40475fa481fe2500cfe6ca
foxmask <olivier@foxmask.info>
2011-01-03 16:01:57 +0100

    issue #77 Migration Process from Hfnu 1.3.x to Hfnu 1.4

---
+++ migration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @package   havefnubb
+* @subpackage havefnubb
+* @author    Olivier Demah
+* @copyright 2010 Olivier Demah
+* @link      http://www.havefnubb.org
+* @license  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
+*/
+require ('havefnubb/application.init.php');
+
+require('lib/installwizard/installWizard.php');
+
+$config = 'havefnubb/migration/wizard.ini.php';
+
+$install = new installWizard($config);
+$install->run(!isAppInstalled());
