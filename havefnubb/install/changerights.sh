3d715c52532cf82467977f2305374dbe541ac155
Laurent Jouanneau <laurent@xulfr.org>
2011-08-21 20:19:35 +0200

    Added a script to easily set rights before the installation, on a linux station

---
+++ havefnubb/install/changerights.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+THIS_PATH=$(dirname $0)
+cd $THIS_PATH/../../
+chmod -R og-w *
+cd havefnubb/var/config
+sudo chown :www-data . defaultconfig.ini.php dbprofils.ini.php havefnubb/config.ini.php havefnubb/flood.coord.ini.php havefnubb/activeusers.coord.ini.php
+chmod g+w . defaultconfig.ini.php dbprofils.ini.php havefnubb/config.ini.php havefnubb/flood.coord.ini.php havefnubb/activeusers.coord.ini.php
+cd ..
+sudo chown -R :www-data log
+chmod -R g+w log
+cd ../..
+sudo chown -R :www-data temp/havefnubb/ cache/ files/
+chmod -R g+w temp/havefnubb/ cache/ files/
