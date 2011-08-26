098ba128c2e44f39cd0d6eb5a815e4163535cd56
Olivier Demah <olivier@foxmask.info>
2011-08-26 22:43:13 +0200

    issue 3 : Users Online Today

---
+++ havefnubb/modules/havefnubb/install/upgrade_to_1.4.1_last_visit.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* @package   havefnubb
+* @subpackage havefnubb
+* @author    FoxMaSk
+* @copyright 2008-2011 FoxMaSk
+* @link      http://havefnubb.org
+* @licence  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
+*/
+
+class havefnubbModuleUpgrader_last_visit extends jInstallerModule {
+
+    function install() {
+        if ($this->firstDbExec()) {
+            $cn = $this->dbConnection();
+            $cn->exec("ALTER TABLE ".$cn->prefixTable('community_users')." ADD last_visit INT(12) DEFAULT '0'");
+            $cn->exec("UPDATE ".$cn->prefixTable('community_users')." SET last_visit = last_post");
+        }
+    }
+}
