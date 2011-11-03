7fb293ef1d55bc3794356fe23b29ec6d00a7ca3b
Laurent Jouanneau <ljouanneau@gmail.com>
2011-11-03 18:20:37 +0100

    added a missing theme.php file for the default960 theme

---
+++ havefnubb/var/themes/default960/theme.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* @package   havefnubb
+* @subpackage hfnuthemes
+* @author    FoxMaSk
+* @copyright 2008-2011 FoxMaSk
+* @link      http://havefnubb.org
+* @licence  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
+*/
+
+$themeInfos = array (
+	'name'=>'Old Default Theme',
+	'createddate'=>'2009-09-01',
+	'label'=>array('en_EN'=>'Old Default Theme',
+				   'fr_FR'=>'Ancien Thème Principal'
+				   ),
+	'description'=>array('en_EN'=>'Old Default theme  of HaveFnuBB! 1.4, using the system <a href="http://960.gs" alt="960 grid system">960gs</a>',
+						 'fr_FR'=>'Ancien Thème Principal de HaveFnuBB! 1.4, utilisant le système <a href="http://960.gs" alt="960 grid system">960gs</a>',
+						),
+	'licence'=>array('url'=>'http://www.gnu.org/licenses/gpl.html',
+					 'description'=>'GNU General Public Licence'
+					 ),
+	'copyright'=>'2008-2011 FoxMaSk',
+	'creator'=>array('name'=>'FoxMaSk','email'=>'foxmask@havefnubb.org','active'=>true),
+	'homepageURL'=>'http://www.havefnubb.org/',
+	'updateURL'=>'http://www.havefnubb.org/update.php?theme=Default960',
+	'version'=>'1.0.1'
+);
