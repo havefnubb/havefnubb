95955c1f2c617a7be16619eee634dcad11320ea5
Laurent Jouanneau <dev@ljouanneau.com>
2019-09-11 22:58:50 +0200

    [hfnucontact] add local configuration

--- src/havefnubb/modules/hfnucontact/install/configure.php
+++ src/havefnubb/modules/hfnucontact/install/configure.php
@@ -9,6 +9,7 @@
  */
 
 use Jelix\Installer\Module\API\ConfigurationHelpers;
+use Jelix\Installer\Module\API\LocalConfigurationHelpers;
 
 class hfnucontactModuleConfigurator extends \Jelix\Installer\Module\Configurator {
 
@@ -22,10 +23,36 @@ class hfnucontactModuleConfigurator extends \Jelix\Installer\Module\Configurator
     {
         $config = $helpers->getConfigIni();
         if ($config->getValue('to_contact', 'hfnucontact') === null) {
-            $config->setValue('to_contact','', 'hfnucontact');
+            $config->setValue('to_contact', '', 'hfnucontact');
         }
         if ($config->getValue('email_contact', 'hfnucontact') === null) {
-            $config->setValue('email_contact','', 'hfnucontact');
+            $config->setValue('email_contact', '', 'hfnucontact');
         }
+    }
+
+    public function localConfigure(LocalConfigurationHelpers $helpers)
+    {
+        $config = $helpers->getConfigIni();
+        $cli = $helpers->cli();
+
+        $email = $cli->askInformation(
+            'The email where to send contact request from user',
+            $config->getValue('email_contact', 'hfnucontact') || '',
+            false,
+            function ($value) {
+                if (jFilter::isEmail($value)) {
+                    return $value;
+                }
+                throw new \Exception('Invalid Email');
+            }
+        );
+        $config->setValue('email_contact', $email, 'hfnucontact');
+
+        $name = $cli->askInformation(
+            'The displayed name of the contact',
+            $config->getValue('to_contact', 'hfnucontact') || '',
+            false
+        );
+        $config->setValue('to_contact', $name, 'hfnucontact');
     }
 }
